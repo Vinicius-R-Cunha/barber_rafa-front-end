b1f0efba09e86743c8a2ea02118b970767b7578c
Vinicius <vinicunha2008@gmail.com>
2022-03-20 04:31:46 -0300

    display grid on servicesSection

--- src/components/ServicesSection/index.js
+++ src/components/ServicesSection/index.js
@@ -1,9 +1,33 @@
-import { ServicesDiv } from "./style";
+import { ServicesDiv, Services, Service } from "./style";
 
 export default function ServicesSection() {
     return (
         <ServicesDiv>
-
+            <p className="services-title">Cortes de Cabelo</p>
+            <Services>
+                <Service>
+                    <p className="service-name">Adulto e adolescente</p>
+                </Service>
+                <Service>
+                    <p className="service-name">Infantil</p>
+                </Service>
+                <Service>
+                    <p className="service-name">Acabamento</p>
+                </Service>
+                <Service>
+                    <p className="service-name">Só Lateral</p>
+                </Service>
+                <Service>
+                    <p className="service-name">Raspado na maquina e navalha</p>
+                </Service>
+            </Services>
+            <Services>
+                <Service></Service>
+                <Service></Service>
+                <Service></Service>
+                <Service></Service>
+                <Service></Service>
+            </Services>
         </ServicesDiv>
     )
 }
--- src/components/ServicesSection/style.js
+++ src/components/ServicesSection/style.js
@@ -3,9 +3,48 @@ import styled from "styled-components";
 const ServicesDiv = styled.div`
     width: 100%;
 
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    gap: 15px;
     padding: 90px 0;
 
     background-color: #FFFFFF;
+
+    .services-title {
+        font-size: 17px;
+        font-weight: 700;
+        text-transform: uppercase;
+    }
 `
 
-export { ServicesDiv }
+const Services = styled.div`
+    width: 1200px;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-column-gap: 30px;
+    grid-row-gap: 40px;
+
+    justify-content: center;
+
+    background-color: green;
+`
+
+const Service = styled.div`
+    width: 380px;
+    height: 150px;
+
+    background-color: lightgreen;
+
+    .service-name {
+        font-size: 18px;
+        font-weight: 700;
+        text-transform: uppercase;
+        color: rgba(0,0,0,.6);
+    }
+`
+
+export { ServicesDiv, Services, Service }
