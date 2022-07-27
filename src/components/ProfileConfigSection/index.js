8398b6f0f216a0758aece79debe82a6a799d8d8b
Vinicius <vinicunha2008@gmail.com>
2022-07-27 17:54:36 -0300

    config page component created

---
+++ src/components/ProfileConfigSection/index.js
@@ -0,0 +1,5 @@
+import { Container } from "./style";
+
+export default function ProfileConfigSection() {
+  return <Container></Container>;
+}
---
+++ src/components/ProfileConfigSection/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 50px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background-color: green;
+`;
