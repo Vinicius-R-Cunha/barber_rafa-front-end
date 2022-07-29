d16344eac1b2072fa13251c3e34872f89cebbf74
Vinicius <vinicunha2008@gmail.com>
2022-07-29 01:06:31 -0300

    oauth button styles update

--- src/components/AuthenticationModal/style.js
+++ src/components/AuthenticationModal/style.js
@@ -265,57 +265,115 @@ export const Spacer = styled.div`
 `;
 
 export const GoogleLogin = styled.div`
-  all: unset;
-
   width: 100%;
-  height: 40px;
 
   font-family: "Montserrat";
   font-size: 0.735rem;
   font-weight: 800;
-  line-height: 1em;
+  line-height: 40px;
   letter-spacing: 0.7px;
   text-transform: uppercase;
-  color: #000000;
+  text-align: center;
+  color: #ffffff;
+  white-space: nowrap;
 
-  background-color: #ffffff;
+  background: #dd4b39;
 
   margin-top: 15px;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  box-sizing: border-box;
+  padding: 0 15px 0 auto;
 
+  border: none;
   border-radius: 4px;
 
+  position: relative;
+
   cursor: pointer;
+
+  :before {
+    content: "";
+
+    width: 40px;
+    height: 100%;
+
+    box-sizing: border-box;
+
+    border-right: #bb3f30 1px solid;
+    background: url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_google.png")
+      7px 9px no-repeat;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  :hover,
+  :focus {
+    background: #e74b37;
+    outline: none;
+  }
+
+  :active {
+    box-shadow: inset 0 0 0 32px rgba(0, 0, 0, 0.1);
+  }
 `;
 
 export const FacebookLogin = styled.div`
-  all: unset;
-
   width: 100%;
-  height: 40px;
 
   font-family: "Montserrat";
   font-size: 0.735rem;
   font-weight: 800;
-  line-height: 1em;
+  line-height: 40px;
   letter-spacing: 0.7px;
   text-transform: uppercase;
-  color: #e1e1e1;
+  text-align: center;
+  color: #ffffff;
+  white-space: nowrap;
+  text-shadow: 0 -1px 0 #354c8c;
 
-  background-color: #4267b2;
+  background: #4c69ba;
+  background-image: linear-gradient(#4c69ba, #3b55a0);
 
   margin-top: 15px;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  box-sizing: border-box;
+  padding: 0 15px 0 auto;
 
+  border: none;
   border-radius: 4px;
 
+  position: relative;
+
   cursor: pointer;
+
+  :before {
+    content: "";
+
+    width: 40px;
+    height: 100%;
+
+    box-sizing: border-box;
+
+    border-right: #364e92 1px solid;
+    background: url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_facebook.png")
+      7px 9px no-repeat;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  :hover,
+  :focus {
+    outline: none;
+    background-color: #5b7bd5;
+    background-image: linear-gradient(#5b7bd5, #4864b1);
+  }
+
+  :active {
+    box-shadow: inset 0 0 0 32px rgba(0, 0, 0, 0.1);
+  }
 `;
 
 export const modalStyles = {
