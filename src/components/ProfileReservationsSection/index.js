2535a1ba4e2acf7da36dd658dd8c054fbcf1a6f4
Vinicius <vinicunha2008@gmail.com>
2022-08-09 10:00:33 -0300

    dayjs get month plus one

--- src/components/ProfileReservationsSection/index.js
+++ src/components/ProfileReservationsSection/index.js
@@ -31,7 +31,7 @@ export default function ProfileReservationsSection({
 
   function getDay(date) {
     const day = date.getDate().toString().padStart(2, "0");
-    const month = date.getMonth().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const year = date.getFullYear().toString().padStart(2, "0");
 
     return `${day}/${month}/${year}`;
